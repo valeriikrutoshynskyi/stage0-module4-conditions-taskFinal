05808d69f168d806c8827f007e177e6410a4e3f2
Valerii Krutoshynskyi <valera9496@gmail.com>
2023-02-05 15:12:19 +0100

    Done 4/1/final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year < 1 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(31);
+            case 4, 6, 9, 11 -> System.out.println(30);
+            case 2 -> {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                    System.out.println(29);
+                } else {
+                    System.out.println(28);
+                }
+            }
+            default -> System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -1,6 +1,20 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
-    public void printCompletelyDivided(int dividend, int divider) {
+    public void printCompletelyDivided(int dividend, int divider){
+        if (divider == 0) {
+            System.out.println("division by zero");
+        }
+        else {
+            int result = dividend / divider;
+            if (dividend == result * divider) {
+                System.out.println("can be divided completely");
+            }
+            else {
+                System.out.println("cannot be divided completely");
+            }
+        }
     }
 }
+
+
